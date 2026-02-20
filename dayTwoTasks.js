c296a94d73a020751fe311fca3aea9dabfb7927d
sharatkatwa <sjkatwa2000@gmail.com>
2026-02-20 20:08:53 +0530

    day 2 dom task complete

--- dayTwoTasks.js
+++ dayTwoTasks.js
@@ -114,3 +114,90 @@ btn4.addEventListener('click',()=>{
   console.log(img_elem.src)
   message4.appendChild(`Image Source: ${img_elem.src}`)
 })
+
+// task 5
+
+let btn5 = document.querySelector('.btn5')
+let bar = document.querySelector('.progress-bar')
+let text = document.querySelector('.progress')
+
+
+bar.style.width = 0
+bar.style.maxWidth = '100%'
+
+let width = 0
+btn5.addEventListener('click', ()=>{
+    width+= (Math.floor(Math.random()*20)+10)
+
+  if(width >= 100){
+    width = 100 
+    text.innerHTML = "Loading Complete"
+  }
+  else{
+    bar.style.width = width+'%'
+    text.innerHTML = `${width}% Complete`
+    }
+})
+
+// Task 6
+
+let btn6 = document.querySelector('.btn6')
+let box_container = document.querySelector('.box-container')
+
+
+let boxId = ['box1', 'box2', 'box3', 'box4', 'box5']
+boxId.forEach((Element, index) => {
+  let box = document.createElement('div')
+  box.style.height = '150px'
+  box.style.width = '150px'
+  box.style.backgroundColor = color[index]
+  box.setAttribute('id', Element)
+  box_container.appendChild(box)
+  
+  let visible = true
+  
+  box.addEventListener('click',() =>{
+    if(visible){
+      box.style.visibility = 'hidden'
+      visible = false
+
+    }
+    // else{
+    //   box.style.visibility = 'visible'
+    //   visible = true
+    // }
+  })
+});
+
+// btn6.addEventListener('click', ()=>{
+//   boxId.forEach(element => {
+//     let box = document.getElementById(element)
+//     box.style.visibility = 'visible'
+//   });
+// })
+
+
+// task 7
+
+let btn7 = document.querySelector('.btn7')
+let num1 = document.querySelector('.num1')
+let num2 = document.querySelector('.num2')
+let num3 = document.querySelector('.num3')
+let winner7 = document.querySelector('.winner7')
+
+
+btn7.addEventListener('click',()=>{
+  let random1 = Math.floor(Math.random()*9+1)
+  let random2= Math.floor(Math.random()*9+1)
+  let random3 = Math.floor(Math.random()*9+1)
+  num1.textContent = random1
+  num2.textContent = random2
+  num3.textContent = random3
+  
+  if(random1 == random2 == random3){
+    winner7.innerHTML= 'You Won!'
+  }
+  else{
+    winner7.innerHTML = 'Try again!'
+  }
+})
